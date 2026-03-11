8c97f7020624cbdc75c6143d3a6477da394cd4b1
Franxyy <curmeevvladislav@gmail.com>
2026-03-11 22:16:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,129 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) {
+  	return false;
+  }
+  for (uint64_t z = 2; z < value; z++) {
+  	if (value % z == 0) {
+  		return false;
+  	}
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 2;
+    }
+    int counter = 1;
+    int num = 2;
+
+    for (int i = 3; counter < n; i+=2) {
+        bool logic = true;
+        for (int del = 3; del< i; del+=2) {
+            if (i % del == 0) {
+                logic = false;
+                break;
+            }
+        }
+        if (logic) {
+            counter++;
+            num = i;
+        }
+    }
+    return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+    if (value < 2) {
+        return 2;
+    }
+    else {
+        int num = value + 1;
+        if (num % 2 == 0 && num > 2) {
+            num++;
+        }
+        while (true) {
+            bool logic = false;
+            if (num < 2) {
+                logic = false;
+            }
+            else if (num == 2) {
+                logic = true;
+            }
+            else if (num % 2 == 0) {
+                logic = false;
+            }
+            else {
+                for (int del = 3; del < num; del += 2) {
+                    if (num % del == 0) {
+                        logic = false;
+                        break;
+                    }
+                }
+            }
+            if (logic) {
+                return num;
+            }
+            num += 2;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    int sum = 2;
+    if (hbound < 2) {
+        return 0;
+    }
+    if (hbound == 2) {
+        return 2;
+    }
+    for (int i = 3; i < hbound; i += 2) {
+        bool logic = true;
+        for (int del = 3; del < i; del += 2) {
+            if (i % del == 0) {
+                logic = false;
+                break;
+            }
+        }
+        if (logic) {
+            sum = sum + i;
+        }
+    }
+    return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+    if (hbound < 3) {
+        return 0;
+    }
+    int counter = 0;
+    for (int i = lbound; i + 2 < hbound; i++) {
+        bool logic1 = true;
+        if (i < 2) {
+            logic1 = false;
+        }
+        for (int del = 2; del < i; del++) {
+            if (i % del == 0) {
+                logic1 = false;
+                break;
+            }
+        }
+        bool logic2 = true;
+        int num2 = i + 2;
+        for (int del = 2; del < num2; del++) {
+            if (num2 % del == 0) {
+                logic2 = false;
+                break;
+            }
+        }
+        if (logic1 && logic2) {
+            counter++;
+        }
+    }
+    return counter;
 }
